66452584e0d44d8f641145e176abd90a782e27c6
juniorsaraviao <junior.saravia.o@uni.pe>
2022-09-07 21:55:25 -0500

    Add fragments

---
+++ app/src/main/java/com/mitocode/marketapp/MenuActivity.kt
@@ -0,0 +1,37 @@
+package com.mitocode.marketapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.mitocode.marketapp.databinding.ActivityMenuBinding
+import kotlinx.android.synthetic.main.activity_menu.*
+
+class MenuActivity : AppCompatActivity() {
+
+    // xml: activity_menu -> activity_menuBinding
+    private lateinit var binding: ActivityMenuBinding
+    private lateinit var fragment: Fragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCourse.setOnClickListener {
+            fragment = CourseFragment()
+            addFragment()
+        }
+
+        binding.btnEnroll.setOnClickListener {
+            fragment = EnrollFragment()
+            addFragment()
+        }
+    }
+
+
+    fun addFragment(){
+        fragment?.let {
+            supportFragmentManager.beginTransaction().replace(R.id.container, fragment).commit()
+        }
+    }
+}
