ff666beba4186b92d1c41577fc75906903c1a874
juniorsaraviao <junior.saravia.o@uni.pe>
2022-09-12 20:31:47 -0500

    Add splash

---
+++ app/src/main/java/com/mitocode/marketapp/examples/InitialHostActivity.kt
@@ -0,0 +1,12 @@
+package com.mitocode.marketapp.examples
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mitocode.marketapp.R
+
+class InitialHostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_initial_host)
+    }
+}
