3110b5e6c4270727052eeb627272ebd3bc4489d3
Junior Saravia <junior.saravia.o@uni.pe>
2022-09-25 22:20:59 -0500

    Use new splash api

--- app/src/main/java/com/mitocode/marketapp/examples/InitialHostActivity.kt
+++ app/src/main/java/com/mitocode/marketapp/examples/InitialHostActivity.kt
@@ -2,11 +2,16 @@ package com.mitocode.marketapp.examples
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.mitocode.marketapp.R
 
 class InitialHostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splash = installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_initial_host)
+
+        Thread.sleep(3000)
+        splash.setKeepOnScreenCondition { false }
     }
 }
