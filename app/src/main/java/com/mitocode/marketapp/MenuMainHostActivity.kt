972ce4f7bbaf9154c433e239e5db86b3ce8c0f11
Junior Saravia <junior.saravia.o@uni.pe>
2022-10-18 23:00:35 -0500

    Add categories

---
+++ app/src/main/java/com/mitocode/marketapp/MenuMainHostActivity.kt
@@ -0,0 +1,13 @@
+package com.mitocode.marketapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MenuMainHostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_main)
+    }
+}
---
+++ app/src/main/java/com/mitocode/marketapp/ui/category/CategoriesFragment.kt
@@ -0,0 +1,17 @@
+package com.mitocode.marketapp.ui.category
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mitocode.marketapp.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class CategoriesFragment : Fragment(R.layout.fragment_categories) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
