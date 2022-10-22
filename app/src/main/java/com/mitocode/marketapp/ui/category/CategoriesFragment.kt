894bdbf88bb9734534d741fcd5f675d147f1accf
Junior Saravia <junior.saravia.o@uni.pe>
2022-10-21 22:31:21 -0500

    Add recycler view

--- app/src/main/java/com/mitocode/marketapp/ui/category/CategoriesFragment.kt
+++ app/src/main/java/com/mitocode/marketapp/ui/category/CategoriesFragment.kt
@@ -6,12 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.mitocode.marketapp.R
+import com.mitocode.marketapp.databinding.FragmentCategoriesBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 //@AndroidEntryPoint
 class CategoriesFragment : Fragment(R.layout.fragment_categories) {
 
+    private lateinit var binding: FragmentCategoriesBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentCategoriesBinding.bind(view)
     }
 }
