3e46c154dfec4842facad699fd5b235efa83b7ff
Junior Saravia <junior.saravia.o@uni.pe>
2022-10-27 22:54:54 -0500

    Update ProductFragment import

--- app/src/main/java/com/mitocode/marketapp/ProductFragment.kt
+++ app/src/main/java/com/mitocode/marketapp/ProductFragment.kt
@@ -1,10 +1,8 @@
 package com.mitocode.marketapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 
 class ProductFragment : Fragment(R.layout.fragment_product) {
