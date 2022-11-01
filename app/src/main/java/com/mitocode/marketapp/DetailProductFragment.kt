e7347fb852c8195c5a5c7dbbf6800b101874a842
Junior Saravia <junior.saravia.o@uni.pe>
2022-10-31 22:47:42 -0500

    Add ProductDetail

--- app/src/main/java/com/mitocode/marketapp/DetailProductFragment.kt
+++ app/src/main/java/com/mitocode/marketapp/DetailProductFragment.kt
@@ -6,20 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
+import com.mitocode.marketapp.databinding.FragmentDetailProductBinding
+import com.squareup.picasso.Picasso
 
 class DetailProductFragment : Fragment(R.layout.fragment_detail_product) {
 
+    private lateinit var binding: FragmentDetailProductBinding
     private val safeArgs: DetailProductFragmentArgs by navArgs()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentDetailProductBinding.bind(view)
 
         init()
     }
 
-    private fun init() {
-        safeArgs.product?.let { product ->  
+    private fun init() = with(binding) {
+        safeArgs.product?.let { product ->
+            tvDescription.text = product.description
+            tvPrice.text = product.price.toString()
+            tvFeatures.text = product.features
 
+            Picasso.get().load(product.images?.get(0)).error(R.drawable.empty).into(imgDetail)
         }
     }
 }
