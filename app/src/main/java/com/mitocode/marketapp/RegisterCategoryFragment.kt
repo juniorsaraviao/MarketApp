c2b633225b2d9704a4afd69ef834ebedbf5922f6
Junior Saravia <junior.saravia.o@uni.pe>
2022-11-04 23:01:42 -0500

    Set btnGallery functionality

--- app/src/main/java/com/mitocode/marketapp/RegisterCategoryFragment.kt
+++ app/src/main/java/com/mitocode/marketapp/RegisterCategoryFragment.kt
@@ -5,10 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import com.mitocode.marketapp.databinding.FragmentRegisterCategoryBinding
 
 class RegisterCategoryFragment : Fragment(R.layout.fragment_register_category) {
 
+    private lateinit var binding: FragmentRegisterCategoryBinding
+
+    val loadImage = registerForActivityResult(ActivityResultContracts.GetContent()){ uri ->
+        binding.imgCategory.setImageURI(uri)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentRegisterCategoryBinding.bind(view)
+
+        events()
+
+    }
+
+    private fun events() = with(binding) {
+        btnCamera.setOnClickListener {
+
+        }
+
+        btnGallery.setOnClickListener {
+            loadImage.launch("image/*")
+        }
     }
 }
