f2b1e519a2476a3c2dc17878f03aaa81e9360cf7
Junior Saravia <junior.saravia.o@uni.pe>
2022-11-04 22:41:11 -0500

    Add RegisterCategoryFragment

---
+++ app/src/main/java/com/mitocode/marketapp/RegisterCategoryFragment.kt
@@ -0,0 +1,14 @@
+package com.mitocode.marketapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class RegisterCategoryFragment : Fragment(R.layout.fragment_register_category) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
