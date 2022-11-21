ffc19e4904731b070f799be0daa145afbb8215fd
Junior Saravia <junior.saravia.o@uni.pe>
2022-11-20 23:26:46 -0500

    Change radiogroup and requests

--- app/src/main/java/com/mitocode/marketapp/data/server/request.kt
+++ app/src/main/java/com/mitocode/marketapp/data/server/request.kt
@@ -29,3 +29,42 @@ data class RegisterCategoryRequest(
     val name: String,
     val cover: String
 )
+
+data class OrderRequest(
+    @SerializedName("direccionEnvio")
+    val direction: DirectionRequest,
+    @SerializedName("metodoPago")
+    val paymentMethod: PaymentMethodRequest,
+    @SerializedName("fechaHora")
+    val dateTime: String,
+    @SerializedName("productos")
+    val products: List<ProductRequest>,
+    val total: Double
+)
+
+data class DirectionRequest (
+    @SerializedName("tipo")
+    val type: Int,
+    @SerializedName("direccion")
+    val direction: String,
+    @SerializedName("referencia")
+    val reference: String,
+    @SerializedName("distrito")
+    val district: String
+)
+
+data class PaymentMethodRequest (
+    @SerializedName("tipo")
+    val type: Int,
+    @SerializedName("monto")
+    val amount: Double
+)
+
+data class ProductRequest (
+    @SerializedName("categoriaId")
+    val categoryId: String,
+    @SerializedName("productoId")
+    val productId: String,
+    @SerializedName("cantidad")
+    val amount: Int
+)
